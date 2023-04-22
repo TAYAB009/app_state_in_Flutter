bdfb070563d53edc73745ddf148174e5cd85e8a8
TAYAB009 <tayabfarooq009@gmail.com>
2023-04-22 17:04:17 +0500

    custom button was reused plus setstate was revised

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  MyAppState createState() => MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class MyAppState extends State {
+  String title = 'My favorite fruit is ';
+  String fruit = 'Unknown';
+  void callback(varFruit) {
     setState(() {
-      _counter++;
+      fruit = varFruit;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title + fruit),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FruitButton(fruit: 'Apple', callback: callback),
+              FruitButton(fruit: 'Oranges', callback: callback),
+              FruitButton(fruit: 'Mangoes', callback: callback),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+}
+
+class FruitButton extends StatelessWidget {
+  final String fruit;
+  final Function callback;
+
+  const FruitButton({required this.fruit, required this.callback, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          callback(fruit);
+        },
+        child: Text(fruit));
   }
 }
